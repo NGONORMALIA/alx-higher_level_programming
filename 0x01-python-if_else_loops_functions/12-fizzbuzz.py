0d8376221d51bafb52e797161b79f52fbb0398ca
NGONORMALIA <maliangonor00@gmail.com>
2024-01-16 16:17:56 -0800

    my first corection

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
     i = 2
-    print("{}".format(1), end='')
+    print("{} ".format(1), end='')
     while i <= 100:
         if i % 5 == 0 and i % 3 == 0:
-            print(" FizzBuzz", end='')
+            print("FizzBuzz ", end='')
         elif i % 3 == 0:
-            print(" Fizz", end='')
+            print("Fizz ", end='')
         elif i % 5 == 0:
-            print(" Buzz", end='')
+            print("Buzz ", end='')
         else:
-            print(" {}".format(i), end='')
+            print("{} ".format(i), end='')
         i += 1
