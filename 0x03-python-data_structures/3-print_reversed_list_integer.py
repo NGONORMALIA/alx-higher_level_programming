e89fdcdce6d2b774a50baaa202db95ee84b81d94
NGONORMALIA <maliangonor00@gmail.com>
2024-01-22 15:52:09 -0800

    f

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-     """Print list in reverse order
+    """Print list in reverse order
 
     Args:
-        my_list: the list to reverse
+    my_list: the list to reverse
 
     Returns:
-        Null
+    Null
     """
     my_list.reverse()
     for i in range(len(my_list)):
