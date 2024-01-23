51d6868a40b41876abfc6aaf10cfdb9db30a97c9
NGONORMALIA <maliangonor00@gmail.com>
2024-01-23 08:44:45 -0800

    too enough

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list)
+    if n == 0 or my_list is None:
+        return None
     for i in range(n):
         print("{:d}".format(my_list[n-i-1]))
