69fab9dfb05d46b87e5e47d142655501fd8730c6
NGONORMALIA <maliangonor00@gmail.com>
2024-02-05 13:08:48 -0800

    try again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         k = 0
         while k < x:
             print("{}".format(my_list[k]), end="")
-            k += my_l
+            k += 1
     except IndexError:
         print("")
         return k
