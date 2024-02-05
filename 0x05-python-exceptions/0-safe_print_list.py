a25deff29198008849c61b984b7583996562e825
NGONORMALIA <maliangonor00@gmail.com>
2024-02-05 13:06:37 -0800

    try again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         k = 0
         while k < x:
-                print("{}".format(my_list[k]), end="")
-                k += 1
+            print("{}".format(my_list[k]), end="")
+            k += my_l
     except IndexError:
         print("")
         return k
