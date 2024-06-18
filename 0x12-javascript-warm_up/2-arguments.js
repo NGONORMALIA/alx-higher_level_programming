149ad113bcea4c72e29197cb5e23a49a5ea557cd
NGONORMALIA <maliangonor00@gmail.com>
2024-06-18 13:10:01 -0700

    third

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv.length;
-/*if (arg >= 3) {
-  console.log('Argument found');
-} else {
-  console.log('No argument');
-}*/
 console.log(arg === 2 ? 'No argument' : arg === 3 ? 'Argument found' : 'Arguments found');
